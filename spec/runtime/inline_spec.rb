39ab3f755e6a2eecc7da500ae3b801028621e401
David Rodríguez <deivid.rodriguez@riseup.net>
2019-11-05 16:37:52 +0100

    Stop allowing warnings on inline specs

--- spec/runtime/inline_spec.rb
+++ spec/runtime/inline_spec.rb
@@ -89,7 +89,6 @@ RSpec.describe "bundler/inline#gemfile" do
 
     expect(out).to include("Installing activesupport")
     err_lines = err.split("\n")
-    err_lines.reject!{|line| line =~ /\.rb:\d+: warning: / }
     expect(err_lines).to be_empty
     expect(exitstatus).to be_zero if exitstatus
   end
